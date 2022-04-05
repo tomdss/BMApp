292bc06b1877e72a98f89d91ab43df145d1cd8fe
liemtt <liemtt@rikkeisoft.com>
2022-04-05 11:54:39 +0700

    Add file

--- app/build/generated/source/dataBinding/trigger/debug/com/example/basemvvm/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.example.basemvvm;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
--- app/build/generated/source/kapt/debug/com/example/basemvvm/BR.java
+++
@@ -1,9 +0,0 @@
-package com.example.basemvvm;
-
-public class BR {
-  public static final int _all = 0;
-
-  public static final int category = 1;
-
-  public static final int movie = 2;
-}
